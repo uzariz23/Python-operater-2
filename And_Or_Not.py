76513f9ac9e643d7d3ef33ce3885c4c806cdd8a6
UzairCoding <ambaraliriza@gmail.com>
2026-01-03 13:40:58 +0000

    operaters

---
+++ And_Or_Not.py
@@ -0,0 +1,14 @@
+a = 10
+b = -10
+c = 0
+
+if a > 0 or b > 0:
+    print("Either number is greater than 0")
+else:
+
+    print("No number is greater than 0")
+
+if b > 0 or c > 0:
+        print("Either of the numbers are greater than 0")
+else:
+        print("no number is greater than 0")
